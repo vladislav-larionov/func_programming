1af94ee8fecef820de76f1e2fec2d8b22c235cf3
Vladislav <vladislav.larionov@fruct.org>
2021-04-07 20:19:35 +0300

    add action labels

--- parse_yarnews_net.py
+++ parse_yarnews_net.py
@@ -10,7 +10,7 @@ class YarnewsNetParser(NewsSiteParser):
     Класс парсера разбирающего сайт https://www.yarnews.net/
     """
 
-    def retrieve_article_text(self, link: str) -> str:
+    def retrieve_article_text(self, link: str) -> str: # Действие
         """
         Получение статьи и выделение из неё нужного текста.
 
@@ -20,14 +20,14 @@ class YarnewsNetParser(NewsSiteParser):
         :return: строка с текстом статьи
         """
         full_text = ''
-        text_parts = super().get_html(link).find('div', class_="text").find_all('p')
+        text_parts = super().get_html(link).find('div', class_="text").find_all('p') # Действие
         for part in text_parts:
             part = part.get_text()
             strong_tag = str(re.search(r'<.strong>', part))
-            full_text = full_text + part.replace(strong_tag, ' ').strip() + '\n'
+            full_text = full_text + part.replace(strong_tag, ' ').strip() + '\n' # Действие
         return full_text
 
-    def parse_news_items(self, items: list, earliest_date: str) -> list:
+    def parse_news_items(self, items: list, earliest_date: str) -> list: # Действие
         """
         Получение требуемой информации о новостях из html кода списка новостей с сайта.
 
@@ -41,10 +41,9 @@ class YarnewsNetParser(NewsSiteParser):
         for item in items:
             title = item.find('a', class_='news-name').get_text()
             link = "https://www.yarnews.net" + item.find('a', class_='news-name').get('href')
-            full_text = self.retrieve_article_text(link)
-            date_time = datetime.strptime(item.find('span', class_="news-date").get_text(),
-                                          "%d.%m.%Y в %H:%M")
-            print(f'{date_time.strftime("%H:%M:%S, %Y-%m-%d")} | {title}', file=sys.stderr)
+            full_text = self.retrieve_article_text(link) # Действие
+            date_time = datetime.strptime(item.find('span', class_="news-date").get_text(), "%d.%m.%Y в %H:%M")
+            print(f'{date_time.strftime("%H:%M:%S, %Y-%m-%d")} | {title}', file=sys.stderr) # Действие
             categories = list()
             if date_time > earliest_date:
                 news.append({
@@ -59,7 +58,7 @@ class YarnewsNetParser(NewsSiteParser):
                 return news, False
         return news, True
 
-    def parse(self, earliest_date: datetime) -> list:
+    def parse(self, earliest_date: datetime) -> list: # Действие
         """
         Обработка данных новостей взятых с сайта "YarNews".
 
@@ -72,9 +71,9 @@ class YarnewsNetParser(NewsSiteParser):
         news_loaded = 0
         more_news_exists = True
         while more_news_exists:
-            request = super().get_html('https://www.yarnews.net/news/chronicle/ajax/' + str(news_loaded) + '/')
+            request = super().get_html('https://www.yarnews.net/news/chronicle/ajax/' + str(news_loaded) + '/') # Действие
             items = request.find_all('div', class_="news-feed-info")
-            news_from_parser, more_news_exists = self.parse_news_items(items, earliest_date)
+            news_from_parser, more_news_exists = self.parse_news_items(items, earliest_date) # Действие
             news = news + news_from_parser
             news_loaded += 30
         return news
